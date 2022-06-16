4f7930b200eeb42eb3cfa4dc08a438091c6ec50e
irfan <irfannakul@gmail.com>
2022-06-16 19:01:45 +0530

    Irfan: June 16 2022: Adding project first time to github.

---
+++ src/main/java/IrfanGroupId/IrfanArtifactId/App.java
@@ -0,0 +1,13 @@
+package IrfanGroupId.IrfanArtifactId;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
