0865a1cad838c825b58e4e22f45f0299bb38645f
Lenovo <Lenovo@DESKTOP-6RLRDCV>
2022-06-23 02:36:28 -0700

    sai : 23 jun 2022 conflict

--- src/main/java/IrfanGroupId/IrfanArtifactId/App.java
+++ src/main/java/IrfanGroupId/IrfanArtifactId/App.java
@@ -9,6 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( " java learning")
+        System.out.println( " java learning");
+        System.out.println("sai");
+        System.out.println("kiran");
     }
 }
